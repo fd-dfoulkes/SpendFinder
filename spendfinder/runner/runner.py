0209a682850b00adc378b88393aaafb702a995c8
fd-dfoulkes <daniel.foulkes@fanduel.com>
2025-02-17 20:59:27 +0000

    format doc.

--- spendfinder/runner/runner.py
+++ spendfinder/runner/runner.py
@@ -2,6 +2,7 @@ import click
 from spendfinder.cmds.report_cloudwatch import logs_costs as logs
 from spendfinder.cmds.report_cloudwatch import metrics_costs as metrics
 
+
 @click.group(chain=True, help="Cloudwatch costs and usage")
 def aws_cloudwatch():
     pass
@@ -14,7 +15,6 @@ def aws_cloudwatch():
 @click.option("--profile", help="AWS profile to use")
 @click.option("--service", help="which cw service to report on")
 def cloudwatch(output, service, profile):
-
     if service == "logs":
         if output:
             if not output.endswith(".csv"):
@@ -24,20 +24,22 @@ def cloudwatch(output, service, profile):
             usage_report = logs(profile)
             usage_report_sorted = sorted(usage_report, key=lambda item: item.get_cost(), reverse=True)
 
-
             for item in usage_report_sorted:
-                click.echo(f"ARN: {item.arn}, Size: {item.get_size_in_gb()} GB, Cost: ${item.get_cost()}, Retention: {item.retention_in_days}, Created: {item.created_time}, Region: {item.region}")
+                click.echo(
+                    f"ARN: {item.arn}, Size: {item.get_size_in_gb()} GB, Cost: ${item.get_cost()}, Retention: {item.retention_in_days}, Created: {item.created_time}, Region: {item.region}")
 
             with open(output, "w") as f:
                 f.write("ARN,Size,Retention,Cost,Created,Region\n")
                 for item in usage_report_sorted:
-                    f.write(f"{item.arn},{item.get_size_in_gb()},{item.retention_in_days},{item.get_cost()},{item.created_time},{item.region}\n")
+                    f.write(
+                        f"{item.arn},{item.get_size_in_gb()},{item.retention_in_days},{item.get_cost()},{item.created_time},{item.region}\n")
 
     if service == "metrics":
         click.echo("Please wait, this can take a while...")
         num_of_metrics, cost = metrics(profile)
         click.clear()
         click.echo(f" total of {num_of_metrics} metrics costing a monthly total of ${cost}")
+
 
 # identify ec2 instances which are on with no traffic.
 
@@ -60,4 +62,3 @@ cli = click.CommandCollection(sources=[aws_cloudwatch])
 
 if __name__ == "__main__":
     cli()
-
